e7969bcb12e59e78dc4366c08336e32678790297
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-04-17 23:34:36 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      border: "hsl(var(--border))",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+      ring: "hsl(var(--ring))",
+      card: "hsl(var(--card))",
+      muted: "hsl(var(--muted))"
+      }
+    },
   },
   plugins: [],
 }
