179ca4292e1f1324838a282615bb433270b537cb
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-04-19 00:40:52 +0100

    Refactor ESLint configuration with new plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,27 @@
-import js from '@eslint/js'
-import ts from 'typescript-eslint'
-import react from 'eslint-plugin-react'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-export default [
-  js.configs.recommended,
-  ...ts.configs.recommended,
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{ts,tsx}'],
-    plugins: { react },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
     rules: {
-      'react/react-in-jsx-scope': 'off',
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
-]
+);
+
