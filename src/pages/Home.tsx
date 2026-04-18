6896c3bf4b8716e8f49903c199d8cab789ce3e1e
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-04-19 00:20:16 +0100

    Add imports for Navbar, Footer, and utility functions

---
+++ src/pages/Home.tsx
@@ -0,0 +1,4 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import { getCityData } from "../lib/cities"
+import { validateItinerary } from "../lib/validation"
