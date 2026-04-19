57b0b4c37abb0a2da34da1ed8fdc4de2bc483b5a
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-04-19 01:01:54 +0100

    Delete src/pages/Home.tsx

--- src/pages/Home.tsx
+++
@@ -1,4 +0,0 @@
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import { getCityData } from "../lib/cities"
-import { validateItinerary } from "../lib/validation"
